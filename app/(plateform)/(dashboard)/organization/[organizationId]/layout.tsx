5b97a9956a0cbcf036eda4255a6031f0853c9338
Mudassir Abbas <mudassir.abbas@allzonetech.com>
2024-03-22 15:36:54 +0500

    change in organization settion

---
+++ app/(plateform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -0,0 +1,10 @@
+import OrgControl from "./_components/org-control";
+
+const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <OrgControl />
+      {children}
+    </div>
+  );
+};
--- app/(plateform)/(dashboard)/organization/layout.tsx
+++ app/(plateform)/(dashboard)/organization/layout.tsx
@@ -1,8 +1,11 @@
 const OrganizationLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="pt-20 md:pt-24 px-4 max-w-6xl 2xl:max-w-screen-xl">
-      <div>{children}</div>
-    </div>
+    <main className="pt-20 md:pt-24 px-4 max-w-6xl 2xl:max-w-screen-xl">
+      <div className="flex gap-x-7">
+        <div className="w-64 shrink-0 hidden md:block"></div>
+        {children}
+      </div>
+    </main>
   );
 };
 export default OrganizationLayout;
