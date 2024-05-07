1b73d26dcf3da1462623b83ac1f208e5d063134a
Mudassir Abbas <mudassir.abbas@allzonetech.com>
2024-05-07 22:45:52 +0500

    complete list options

---
+++ actions/delete-list/index.ts
@@ -0,0 +1,40 @@
+"use server";
+
+import { auth } from "@clerk/nextjs";
+import { revalidatePath } from "next/cache";
+
+import { InputType, ReturnType } from "./types";
+import { db } from "@/lib/db";
+import { createSafeAction } from "@/lib/create-safe-action";
+import { DeleteList } from "./schema";
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return { error: "unauthorized" };
+  }
+
+  const { id, boardId } = data;
+
+  let list;
+  try {
+    list = await db.list.delete({
+      where: {
+        id,
+        boardId,
+        board: {
+          orgId,
+        },
+      },
+    });
+  } catch (error) {
+    return {
+      error: "failed to delete",
+    };
+  }
+  revalidatePath(`/board/${boardId}`);
+  return { data: list };
+};
+
+export const deleteList = createSafeAction(DeleteList, handler);
