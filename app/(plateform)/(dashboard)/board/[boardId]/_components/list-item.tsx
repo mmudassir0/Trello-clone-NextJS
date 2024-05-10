34cf50ea0ff7e7b883350ce269a13d6e0ec73137
Mudassir Abbas <mudassir.abbas@allzonetech.com>
2024-05-10 23:56:17 +0500

    complete drag and drop

--- app/(plateform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(plateform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { ElementRef, useRef, useState } from "react";
+import { Draggable, Droppable } from "@hello-pangea/dnd";
+
 import { ListWithCards } from "@/types";
 import ListHeader from "./list-header";
 import CardForm from "./card-form";
@@ -27,29 +29,46 @@ const ListItem = ({ index, data }: ListItemProps) => {
     });
   };
   return (
-    <li className="shrink-0 h-full w-[272px] select-none">
-      <div className=" w-full rounded-md bg-[#f1f2f4] shadow-md pb-2">
-        <ListHeader onAddCard={enableEditing} data={data} />
-
-        <ol
-          className={cn(
-            "flex flex-col gap-y-2 px-1 py-0.5 mx-1",
-            data.cards.length > 0 ? "mt-2" : "mt-0"
-          )}
+    <Draggable draggableId={data.id} index={index}>
+      {(provided) => (
+        <li
+          {...provided.draggableProps}
+          ref={provided.innerRef}
+          className="shrink-0 h-full w-[272px] select-none"
         >
-          {data.cards.map((card, index) => (
-            <CardItem index={index} key={card.id} data={card} />
-          ))}
-        </ol>
-        <CardForm
-          listId={data.id}
-          ref={textareaRef}
-          isEditing={isEditing}
-          enableEditing={enableEditing}
-          disableEditing={disableEditing}
-        />
-      </div>
-    </li>
+          <div
+            {...provided.dragHandleProps}
+            className=" w-full rounded-md bg-[#f1f2f4] shadow-md pb-2"
+          >
+            <ListHeader onAddCard={enableEditing} data={data} />
+            <Droppable droppableId={data.id} type="card">
+              {(provided) => (
+                <ol
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                  className={cn(
+                    "flex flex-col gap-y-2 px-1 py-0.5 mx-1",
+                    data.cards.length > 0 ? "mt-2" : "mt-0"
+                  )}
+                >
+                  {data.cards.map((card, index) => (
+                    <CardItem index={index} key={card.id} data={card} />
+                  ))}
+                  {provided.placeholder}
+                </ol>
+              )}
+            </Droppable>
+            <CardForm
+              listId={data.id}
+              ref={textareaRef}
+              isEditing={isEditing}
+              enableEditing={enableEditing}
+              disableEditing={disableEditing}
+            />
+          </div>
+        </li>
+      )}
+    </Draggable>
   );
 };
 
